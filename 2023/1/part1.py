0ec416e438eb694f152dfa421795901079b5befe
n-sweep <34486798+n-sweep@users.noreply.github.com>
2023-12-01 10:40:34 -0500

    2023 day 1

---
+++ 2023/1/part1.py
@@ -0,0 +1,23 @@
+
+with open('input.txt', 'r') as f:
+    lines = f.read().strip().split('\n')
+
+
+def get_digits(string):
+    for char in string:
+        if char.isdigit():
+            return char
+
+
+def main():
+    calibration_values = []
+    for line in lines:
+        first = get_digits(line)
+        last = get_digits(line[::-1])
+
+        calibration_values.append(int(first + last))
+
+    print(sum(calibration_values))
+
+if __name__ == '__main__':
+    main()
---
+++ 2023/t/README.md
@@ -0,0 +1,21 @@
+# AoC Day 
+    https://adventofcode.com/2023/day/#
+
+```sh
+pst > input.txt
+pst > example1.txt
+cp part1.py part2.py
+pst > example2.txt
+```
+
+```sh
+# exit
+ipython --no-autoindent
+%load_ext autoreload
+%autoreload 2
+clear
+```
+
+## Part 1
+
+## Part 2
