24cde797196d3cbc413bcde62905d09912737d29
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-12-04 17:08:09 -0500

    day 4 pt 2

--- 2024/4/day4.py
+++ 2024/4/day4.py
@@ -3,18 +3,18 @@
 
 import re
 import numpy as np
+from itertools import product
+
 
 with open('puzzle.txt', 'r') as f:
-    data = f.read().strip()
+    arr = np.array([list(r.strip()) for r in f.readlines()])
 
 
 def pt1():
-    arr = np.array([list(r.strip()) for r in data.splitlines()])
-
     diag = []
     for i in range(-arr.shape[0], arr.shape[0]):
         for a in [arr, np.fliplr(arr)]:
-            diag.append(np.pad(np.diagonal(a, i), (0, abs(i)), 'empty'))
+            diag.append(np.pad(np.diag(a, i), (0, abs(i)), 'empty'))
 
     diag = np.array(diag)
     search = '\n'.join([''.join(row) for row in [*arr, *arr.T, *diag]])
@@ -23,7 +23,20 @@ def pt1():
 
 
 def main():
-    pt1()
+    total = 0
+    for y, x in list(zip(*np.where(arr=='A'))):
+        win = arr[y-1:y+2, x-1:x+2]
+
+        if tuple(win.shape) != (3, 3):
+            continue
+
+        d1 = ''.join(np.diag(win))
+        d2 = ''.join(np.diag(np.fliplr(win)))
+
+        if d1 in ['MAS', 'SAM'] and d2 in ['MAS', 'SAM']:
+            total += 1
+
+    print(total)
 
 
 if __name__ == '__main__':
